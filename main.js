f64a0837f20b2518f9d9a39d3b773f4cb49f4b78
AndreanLNA <83387525+AndreanLNA@users.noreply.github.com>
2021-09-19 22:43:03 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+function setup(){
+	
+	pinMode(0, INPUT);
+	pinMode(1, OUTPUT); // output bernilai HIGH dan LOW
+	
+}
+
+function loop(){ // membuat sebuah kondisi
+
+	var motion = digitalRead(0); // untuk mendeklarasikan motion sebagai inputan.
+	Serial.println(motion); // mengeluarkan data motion.
+	
+	if (motion == 1023){ 
+		digitalWrite(1, HIGH) // jika terjadi gerakan dari cursor mouse maka akan menyalakan lampu.
+	}
+	
+	else {
+		digitalWrite(1, LOW); // jika tidak terdapat gerakan maka lampu akan mati.
+	}
+}
